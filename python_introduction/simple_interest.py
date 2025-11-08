7882e5b20d3d8e59f615ea1dbfb7841362290f1a
Jamiil007 <oussama.jamil.11@gmail.com>
2025-11-08 16:00:14 +0100

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Defining the variables
+principal = 1000 #representing $1000
+rate = 5 #representing 5% annual interest rate
+time = 3 #representing 3 years
+
+#Calculating simple interest
+simple_interest = (principal * rate * time) / 100
+
+#Printing the result
+print(f"The simple interest is: {simple_interest}")
