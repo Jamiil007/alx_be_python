a17e5fcae29cfa06ba9cabbbf4fdfdcf11cf05be
Jamiil007 <oussama.jamil.11@gmail.com>
2025-11-17 15:37:52 +0100

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,9 +12,10 @@ def perform_operation(num1, num2, operation):
             return num1 / num2
         case _:
             return "Error: Invalid operation"
+
 def main():
     num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     result = perform_operation(num1, num2, operation)
