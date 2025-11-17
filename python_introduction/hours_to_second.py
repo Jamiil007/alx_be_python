6d2b1fa20979deb3b4201f5fa95579899d3a2011
Jamiil007 <oussama.jamil.11@gmail.com>
2025-11-17 15:49:03 +0100

    Remove hours_to_second.py

--- python_introduction/hours_to_second.py
+++
@@ -1,8 +0,0 @@
-#Defining the variables
-hours = 2
-
-#Calculating the number of seconds in the given hours
-seconds = hours * 3600
-
-#Printing the result
-print(f"{hours} hour(s) is equal to {seconds} seconds.")
