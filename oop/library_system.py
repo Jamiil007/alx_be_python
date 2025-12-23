1e164e0e8e44a76d047cb59702810851f2b57bc2
Jamiil007 <oussama.jamil.11@gmail.com>
2025-12-23 15:44:33 +0100

    Moree tricks

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,14 +1,39 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.year = year
 
-    def __del__(self):
-        print(f"Deleting {self.title}")
 
-    def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
 
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(
+                    f"EBook: {book.title} by {book.author}, "
+                    f"File Size: {book.file_size}KB"
+                )
+            elif isinstance(book, PrintBook):
+                print(
+                    f"PrintBook: {book.title} by {book.author}, "
+                    f"Page Count: {book.page_count}"
+                )
+            else:
+                print(f"Book: {book.title} by {book.author}")
